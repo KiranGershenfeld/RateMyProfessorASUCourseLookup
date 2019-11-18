acbf848b079bac9747476907a44dcd8330ecdc8d
Kiran Gershenfeld <kiran@gershenfeld.org>
2019-11-18 11:53:41 -0700

    Add files via upload

---
+++ ASUCourseScraper.py
@@ -0,0 +1,87 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import os
+
+#Initialization Information
+TeachersWithoutEntries = []
+TeachersWithNoReviews = []
+text_file = open("Output.txt", "w")
+text_file.write("The Following Data was collected from RateMyProfessor.com as well as the ASU Course Catalog using the Department and Class Number that you entered.")
+
+
+def CreateTeacherList():
+    ProfessorsTeachingThisClass = []
+    #Prompt User for Department and Class
+    userDepartment = input("Department (3 Letters): ")
+    userClassNumber = input("Class Number (3 Numbers): ")
+    #Query ASU Course Catalog for Teachers teaching this class
+    url = "https://webapp4.asu.edu/catalog/myclasslistresults?t=2201&s=" + userDepartment + "&n=" + userClassNumber + "&hon=F&promod=F&c=TEMPE&e=open&page=1&col=D&ord=desc"
+    html = requests.get(url)
+    soup = BeautifulSoup(html.content, "lxml")
+
+    for tags in soup.findAll("a", {"class":"nametip"}):
+        #teacherName = tags.get('title')
+        #teacherName = teacherName[11:]
+        teacherName = tags.text.strip()
+        if teacherName not in ProfessorsTeachingThisClass and teacherName != "Staff":
+            ProfessorsTeachingThisClass.append(teacherName)
+            numASUProfessors = len(ProfessorsTeachingThisClass)
+    return ProfessorsTeachingThisClass
+
+
+#Use this function to get the quality of professors teaching a class from RMP
+def GetProfessorData(professor):
+    QueryURL = "https://solr-aws-elb-production.ratemyprofessors.com//solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q=" + professor + "+AND+schoolid_s%3A45&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s&fq="
+    wrappedPage = requests.get(QueryURL)
+    wrappedJSON = BeautifulSoup(wrappedPage.content, "lxml").find("p").text.strip()
+    trimmedJSON = wrappedJSON[5:-1]
+    JSONdata = json.loads(trimmedJSON)
+    response = JSONdata["response"]
+    return response
+
+
+#Iterates through the list of professors gathered from the ASU course catalog
+def GetRMPData():
+    ProfessorsTeachingThisClass = CreateTeacherList()
+    for professor in ProfessorsTeachingThisClass:
+        response = GetProfessorData(professor)
+        #Conditions for whether professor was found and has reviews
+        if response["numFound"] > 0:
+            profData = response["docs"][0]
+            if profData["total_number_of_ratings_i"] > 0:
+                #Gathering name and id of professor
+                profID = profData["pk_id"]
+                profName = profData["teacherfirstname_t"] + " " + profData["teacherlastname_t"]
+                #Ask Ratemyprofessor for data for this teacher
+                actualRMPURL = requests.get("https://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + str(profID) + "&showMyProfs=true")
+                RMPSoup = BeautifulSoup(actualRMPURL.content, "lxml")
+
+                text_file.write("\n\n" + profName +"\n")
+                text_file.write("https://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + str(profID) + "&showMyProfs=true\n")
+                description = ["Overall Quality", "Would Take Again", "Difficulty"]
+                descriptionNumber = 0
+                for grade in RMPSoup.findAll("div", {"class":"grade"}):
+                    print(description[descriptionNumber] + ": " + grade.text.strip())
+                    text_file.write(description[descriptionNumber] + ": " + grade.text.strip() + "\n")
+                    descriptionNumber+=1
+            else:
+                TeachersWithNoReviews.append(professor)
+        else:
+            TeachersWithoutEntries.append(professor)
+    print("")
+    text_file.write("\n\nThe Following Teachers Had 0 Reviews\n")
+    for teacher in TeachersWithNoReviews:
+        print(teacher)
+        text_file.write(teacher + "\n")
+    print("")
+    text_file.write("\nThe Following Teachers Couldnt Be Found\n")
+    for teacher in TeachersWithoutEntries:
+        print(teacher)
+        text_file.write(teacher + "\n")
+
+    text_file.close()
+    os.startfile("Output.txt")
+
+#Run Project
+GetRMPData()
