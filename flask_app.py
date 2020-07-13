8d7fd8654b39b8e8df0f48e097c4519fd46edd33
Kiran Gershenfeld <kiran@gershenfeld.org>
2020-07-13 01:07:07 -0700

    added config for flask_app

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request, redirect
 from flask import jsonify, make_response
 import ASUCourseScraper as scraper
 import sys
+import os
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 #Routes-------------------
 #Main page redirects to portfolio
 @app.route('/')
