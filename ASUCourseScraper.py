55ead3aefbc2a918126e5cbe8cad4a63fa5c8a2f
Kiran Gershenfeld <kiran@gershenfeld.org>
2020-07-19 17:14:35 -0700

    Code Cleaning, Responsize text sizing

--- ASUCourseScraper.py
+++ ASUCourseScraper.py
@@ -3,10 +3,6 @@ import requests
 import json
 import lxml
 
-#Initialization Information
-TeachersWithoutEntries = []
-TeachersWithNoReviews = []
-
 def CreateTeacherList(inputURL):
     ProfessorsTeachingThisClass = []
     #Asks user for the url of the ASU Course Catalouge page that they want queried
@@ -15,20 +11,14 @@ def CreateTeacherList(inputURL):
     soup = BeautifulSoup(html.content, "lxml")
 
     for tags in soup.findAll("a", {"class":"nametip"}):
-        #teacherName = tags.get('title')
-        #teacherName = teacherName[11:]
-        #teacherName = tags.text.strip()
-        teacherName = tags.get('title', 'NoTitleFound')
-        teacherName = teacherName.split("|")[1]
-        teacherName = teacherName.replace(" ", "%20")
+        teacherName = tags.get('title', 'NoTitleFound').split("|")[1].replace(" ", "%20")
         if teacherName not in ProfessorsTeachingThisClass and teacherName != "Staff":
             ProfessorsTeachingThisClass.append(teacherName)
-            numASUProfessors = len(ProfessorsTeachingThisClass)
     #print(ProfessorsTeachingThisClass)
     return ProfessorsTeachingThisClass
 
 #Use this function to get RMP teacher ids for a given professor name. This is necessary to find their actual RMP page.
-def GetProfessorData(professor):
+def FindMatchingProfessors(professor):
     #This URL is a JSON page for RMP teacher data
     QueryURL = "https://solr-aws-elb-production.ratemyprofessors.com//solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q=" + professor + "+AND+schoolid_s%3A45&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s&fq="
     wrappedPage = requests.get(QueryURL)
@@ -40,7 +30,7 @@ def GetProfessorData(professor):
 
     return response
 
-def QueryRMP(profData):
+def GetReviewsForProfessor(profData):
     #--THIS IS WHERE THE ACTUAL DATA IS FOUND--
     condensedProfData = {}
     #Gathering name and id of professor
@@ -72,7 +62,6 @@ def QueryRMP(profData):
     #Add number of reviews to dictionary
     reviewDiv = RMPSoup.find("div", {"class":"RatingValue__NumRatings-qw8sqy-0 jvzMox"})
     numberOfReviews = reviewDiv.find("a").text.strip()
-    print(numberOfReviews)
     numberOfReviews = numberOfReviews.replace('\xa0', ' ') #Replaces BS4 space with an actual space so that the JSON can be parsed properly
     condensedProfData["numberOfReviews"] = numberOfReviews
 
@@ -84,36 +73,31 @@ def QueryRMP(profData):
     #--END DATA GATHERING--
     return condensedProfData
 
-#Iterates through the list of professors gathered from the ASU course catalog
+#MAIN FUNCTION -- TAKE ASU COURSE SEARCH URL AND RETURNS JSON DATA
 def GetRMPData(inputURL):
     ProfessorsTeachingThisClass = CreateTeacherList(inputURL)
-    RMPData = {} #This will be the JSON returned to the webpage
-    RMPData["Professors"] = {}
-    RMPData["NoReviews"] = {}
-    RMPData["NoEntries"] = {}
+    RMPData = {"Professors":{}, "NoEntries":{}} #This will be the JSON returned to the webpage
 
     for professor in ProfessorsTeachingThisClass:
-        response = GetProfessorData(professor)
+        response = FindMatchingProfessors(professor)
         #Conditions for whether professor was found
         if response["numFound"] > 0:
             profData = response["docs"][0]
-            RMPData["Professors"][profData["teacherfirstname_t"] + " " + profData["teacherlastname_t"]] = QueryRMP(profData)
+            RMPData["Professors"][profData["teacherfirstname_t"] + " " + profData["teacherlastname_t"]] = GetReviewsForProfessor(profData)
+        #If they werent found, query just the last name to find a potetial url
         else:
-            #IF they werent found, query just the last name to find a potetial url
-            secondName = professor.split('%20')[1]
-            secondResponse = GetProfessorData(secondName)
+            secondResponse = FindMatchingProfessors(professor.split('%20')[1])
             if(secondResponse["numFound"] > 0):
                 secondProfData = secondResponse["docs"][0]
-                url = QueryRMP(secondProfData)["RateMyProfessorURL"]
+                url = GetReviewsForProfessor(secondProfData)["RateMyProfessorURL"]
                 RMPData["NoEntries"][professor] = {"name": professor.replace("%20", " "), "status": "Couldnt Be Found ", "url": url}
+            #if no potential url was found, just send back the name
             else:
-                #if no potential url was found, just send back the name
                 tempDict = {professor: "Couldnt be found"}
                 RMPData["NoEntries"][professor] = {"name": professor.replace("%20", " "), "status": "Couldnt be Found", "url": "none"}
 
-
-    print(json.dumps(RMPData, indent=2))
-    return RMPData
+    print("Version--1.0")
+    return RMPData #JSON with professors found, and ones with no entries
 
 #Run Project
 #GetRMPData("https://webapp4.asu.edu/catalog/classlist?t=2207&s=CSE&n=110&hon=F&promod=F&e=open&page=1")
